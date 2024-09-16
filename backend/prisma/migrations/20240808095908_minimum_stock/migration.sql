e9331b592506ce842735c922412aff456a339a01
Tacite243 <taciteiragi@gmail.com>
2024-09-16 23:24:16 +0200

    Mise à jour des fichiers supprimés

--- backend/prisma/migrations/20240808095908_minimum_stock/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "minimumStock" INTEGER NOT NULL DEFAULT 2;
--- frontend/src/components/bouton.jsx
+++
@@ -1,8 +0,0 @@
-
-
-
-export default function DefaultAppBouton({type, text, onClick}){
-    return(
-        <button type={type} onClick={onClick}>{text}</button>
-    )
-}
